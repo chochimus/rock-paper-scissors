db74f141f03f6fa2d3e62879e0bda4e790ef83b7
chochimus <127354521+chochimus@users.noreply.github.com>
2023-08-17 18:20:07 -0400

    Add functionality to play single round

--- javascript.js
+++ javascript.js
@@ -9,3 +9,21 @@ function getComputerChoice(){
       return "scissors";
   }
 }
+
+function playRound(playerSelection, computerSelection){
+  if(computerSelection == playerSelection
+  || computerSelection == playerSelection
+  || computerSelection == playerSelection ){
+    console.log("Tie!");
+  } else if(computerSelection == "rock" && playerSelection == "scissors"
+  || computerSelection == "paper" && playerSelection == "rock" 
+  || computerSelection == "scissors" && playerSelection == "paper"){
+    console.log(`You lose! ${computerSelection} beats ${playerSelection}`);
+  } else{
+    console.log(`You win! ${playerSelection} beats ${computerSelection}`);
+  }
+}
+
+const playerSelection = "rock".toLowerCase();
+const computerSelection = getComputerChoice();
+console.log(playRound(playerSelection, computerSelection));
